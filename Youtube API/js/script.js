1282137dbf6aa5a1765db505b5fedd1bde796947
Keone Martin <keonemartin@gmail.com>
2016-07-06 13:59:11 +0200

    updated the youtube api app to avoid repeat code and also fix the https issue

--- Youtube API/js/script.js
+++ Youtube API/js/script.js
@@ -32,11 +32,12 @@ $(function(){
 
 
 
-function search(){
+function search(token){
   // clear the results section
   $('#results').html('');
   $('#buttons').html('');
 
+  console.log(token);
   // get form input
    q = $('#query').val();
 
@@ -45,6 +46,7 @@ function search(){
     "https://www.googleapis.com/youtube/v3/search", {
       part: 'snippet, id',
       q: q,
+      pageToken: token, //if the token is a truthy value set it as a token for the search.
       type:'video',
       key: 'AIzaSyBEqFcdanQfzc5_GVclqcVFaL1bkn0PKqc'},
       function(data){
@@ -74,92 +76,15 @@ function search(){
 
 function nextPage(){
 	var token = $('#next-button').data('token');
-	var q = $('#next-button').data('query');
-
-	// Clear Results
-	$('#results').html('');
-	$('#buttons').html('');
-
-	// Get Form Input
-	q = $('#query').val();
-
-	// Run GET Request on API
-	$.get(
-		"https://www.googleapis.com/youtube/v3/search",{
-			part: 'snippet, id',
-			q: q,
-			pageToken: token,
-			type:'video',
-			key: 'AIzaSyBEqFcdanQfzc5_GVclqcVFaL1bkn0PKqc'},
-			function(data){
-				var nextPageToken = data.nextPageToken;
-				var prevPageToken = data.prevPageToken;
-
-				// Log Data
-				console.log(data);
-
-				$.each(data.items, function(i, item){
-					// Get Output
-					var output = getOutput(item);
-
-					// Display Results
-					$('#results').append(output);
-				});
-
-				var buttons = getButtons(prevPageToken, nextPageToken);
-
-				// Display Buttons
-				$('#buttons').append(buttons);
-			}
-	);
+  search(token);
 }
-
 
 function prevPage(){
 
   var token = $('#prev-button').data('token');
-  var q = $('#prev-button').data('query');
-  // clear the results section
-  $('#results').html('');
-  $('#buttons').html('');
-
-  // get form input
-  q = $('#query').val();
-
-
-  // do some getting from the API
-  $.get(
-    "https://www.googleapis.com/youtube/v3/search", {
-      part: 'snippet, id',
-      q: q,
-      pageToken: token,
-      type:'video',
-      key: 'AIzaSyBEqFcdanQfzc5_GVclqcVFaL1bkn0PKqc'},
-      function(data){
-        var nextPageToken = data.nextPageToken;
-        var prevPageToken = data.prevPageToken;
-
-        //log the data
-        console.log(data);
-
-        $.each(data.items, function(i, item){
-          // get the output
-          var output = getOutput(item);
-
-          // put the results in the page
-          $('#results').append(output);
-
-        });
-
-        var buttons = getButtons(prevPageToken, nextPageToken);
-
-        //display the buttons
-        $('#buttons').append(buttons);
-
-    }
-  );
-
+  search(token);
 }
+
 //build the output
 function getOutput(item){
   var videoId = item.id.videoId;
@@ -175,7 +100,7 @@ function getOutput(item){
   '<img src="' + thumb + '">' +
   '</div>' +
   '<div class="list-right">' +
-  '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/' + videoId + '">' + title + '</a></h3>' +
+  '<h3><a class="fancybox fancybox.iframe" href="https://www.youtube.com/embed/' + videoId + '">' + title + '</a></h3>' +
   '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate +'</small>' +
   '<p>' + description + '</p>' +
   '</div>' +
