f2b548653712fdd99228164ea7b2105f49447235
Keone Martin <keonemartin@gmail.com>
2016-07-11 17:20:26 +0200

    added the photo gallery app

--- Portfolio Gallery/js/script.js
+++ Portfolio Gallery/js/script.js
@@ -25,4 +25,40 @@ $(document).ready(function(){
     }
     return false; //prevent standard link behaviour.
   });
+
+
+// mouseenter handler for the over lay
+  $('ul#gallery li').on('mouseenter', function(){
+    // get data attribute alues;
+    var title = $(this).children().data('title');
+    var desc = $(this).children().data('desc');
+
+    // validate it
+    if(desc === null){
+      desc = 'Click to enlarge';
+    }
+    if(title === null){
+      title = '';
+    }
+
+    //create the overlay div
+    $(this).append('<div class="overlay"></div>');
+
+    //get the div
+    var overlay = $(this).children('.overlay');
+
+    //Add html to overlay
+    overlay.html('<h3>' + title + '</h3><p>' + desc + '</p>');
+    // make it visible
+    overlay.fadeIn(800);
+
+  });
+
+  //mouse leave overlay event
+  $('ul#gallery li').on('mouseleave', function(){
+    var overlay = $(this).children('.overlay');
+
+    overlay.fadeOut(500);
+    overlay.remove();
+  });
 });
